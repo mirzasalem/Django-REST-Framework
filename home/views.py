a45444298934c38629d3c37391d9f6d25cab1bd0
mirzasalem <mirza.salem2016@gmail.com>
2026-01-12 04:18:58 +0600

    first commit

---
+++ home/views.py
@@ -0,0 +1,66 @@
+# from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Person
+from home.serializers import PeopleSerializer
+
+# person.objects.all()
+
+
+@api_view(['GET', 'POST', 'PUT'])
+def index(request):
+    courses = {
+        'course_name' : 'Python',
+        'learn' : ['flask', 'Django', 'FastAPI', 'Gp'],
+        'course_provider' : 'Mirza'
+    }
+        
+    if request.method == 'GET':
+        print ("Get")
+        return Response(courses)
+    elif request.method == 'POST':
+        data = request.data
+        print (data['age'])
+        print (data['name'])
+        return Response(courses)
+    elif request.method == 'PUT':
+        print ("PUt")
+        return Response(courses)
+    
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def person(request):
+    if request.method == 'GET':
+        object = Person.objects.all()
+        serializer = PeopleSerializer(object , many = True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    elif request.method == 'PUT':
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    elif request.method == 'PATCH':
+        data = request.data
+        object = Person.objects.get(id = data['id'])
+        serializer = PeopleSerializer(object, data = data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    else:
+        data = request.data
+        object = Person.objects.get(id = data['id'])
+        object.delete()
+        return Response("Deleted")
+
+        
